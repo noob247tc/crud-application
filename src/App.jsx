f4048719292a433a3ff761859165a2c77703503e
Justine Samson <justinesamson5tc@gmail.com>
2026-09-19 17:44:44 +0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const [notes, setNotes] = useState([]);
 
 const addNote = () =>{
   const newNote = {
@@ -11,14 +12,26 @@ const addNote = () =>{
     title,
     body
   }
-}
 
+  setNotes([...notes, newNote])
+  setTitle('')
+  setBody('')
+}
   return (
    <>
    <h1> My Note</h1>
    <input placeholder='Title' value ={title} onChange={e => setTitle(e.target.value)}/>
    <textarea placeholder='Write your note...' value = {body} onChange={e => setBody(e.target.value)}></textarea>
-   <button>Add Note</button>
+   <button onClick={addNote}>Add Note</button>
+   <div>
+    {notes.map(note =>(
+      <div key={note.id}>
+        <h3>{note.title}</h3>
+        <p>{note.body}</p>
+      </div>
+      )
+    )}
+   </div>
    </>
   )
 }
